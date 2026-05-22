5ee17be85bc8ecde44be227b4a166fff9effc555
Juan Pablo <diazbetancur@gmail.com>
2026-05-22 10:59:50 -0500

    Update environment.qa.ts

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -5,7 +5,7 @@ export const environment: AppEnvironment = {
   name: 'qa',
   runtimeConfigPath: '/config.qa.json',
   enableServiceWorker: true,
-  defaultApiBaseUrl: 'https://qa-api.example.com/api/v1',
+  defaultApiBaseUrl: 'https://schedule-back-noq9.onrender.com/api/v1',
   defaultAppName: 'Barbershop QA',
   defaultAppUrl: 'https://qa.example.com',
   defaultAssetsBaseUrl: 'https://qa.example.com/assets',
